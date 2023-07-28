2d9c186e26d73665cd6dddf69049c5d10d6b530b
Sarang Bhimanwar <sarangbhimanwar5@gmail.com>
2023-07-28 20:48:40 +0530

    done2

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 const Button = ()=>{
 
-    let[count ,setCount] = useState(1) ;
+    let[count ,setCount] = useState(0) ;
 
     function increaseCount(){
         setCount(count++) ;
